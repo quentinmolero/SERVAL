b0c30fd268a393a4f2d618526eb931e8c805a719
Quentin MOLERO <quentin.molero@estiam.com>
2021-04-02 19:02:25 +0200

    1 menu maxi best of: -boite de 9 nuggets -sauce curry x2 -potatoes -coca avec 1 boite de 6 nuggets sauce curry

--- src/main/java/fr/serval/Main.java
+++ src/main/java/fr/serval/Main.java
@@ -1,10 +1,25 @@
 package fr.serval;
 
 import fr.serval.launcher.Launcher;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(GlobalKeys.SERVAL_NAME_DESC);
-        Launcher.main();
+public class Main extends Application {
+//    public static void main(String[] args) {
+//        System.out.println(GlobalKeys.SERVAL_NAME_DESC);
+//        Launcher.main();
+//    }
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        StackPane root = new StackPane();
+        Button button = new Button("Click me");
+
+        Scene scene = new Scene(root, 600, 400);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
