79042a9ed62723b08c97b536893abdf461d2bdb5
Quentin MOLERO <quentin.molero@estiam.com>
2021-04-08 16:14:31 +0200

    Init package and controller for ticket view

---
+++ src/main/java/fr/serval/application/ticket/TicketController.java
@@ -0,0 +1,16 @@
+package fr.serval.application.ticket;
+
+import fr.serval.controller.Controller;
+import fr.serval.ihm.IHMComponentBuilder;
+
+public class TicketController implements Controller, IHMComponentBuilder {
+    @Override
+    public void setupComponent() {
+
+    }
+
+    @Override
+    public Object getComponent() {
+        return null;
+    }
+}
