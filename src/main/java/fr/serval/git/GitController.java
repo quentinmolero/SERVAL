93bd9dc925115ecc5800d5075a70aa655526e660
QuentinR <rouvilleq@gmail.com>
2021-05-02 21:19:12 +0200

    Add login method

--- src/main/java/fr/serval/git/GitController.java
+++ src/main/java/fr/serval/git/GitController.java
@@ -1,19 +1,18 @@
 package fr.serval.git;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import org.json.simple.JSONArray;
+
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class GitController {
-    private final String ACCESS_TOKEN = "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";
-    private final String USER_NAME = "JackAuxLanternes";
+    private String access_token;
+    private String session;
+    private String user_name;
 
     private static GitController instance;
 
@@ -21,7 +20,6 @@ public class GitController {
         if (isInstanceNotInitialized()) {
             instance = new GitController();
         }
-
         return instance;
     }
 
@@ -40,41 +38,44 @@ public class GitController {
         return instance == null;
     }
 
-    public void getAllRepositoriesFromUser() throws ParseException, IOException {
-        String contentString = getUserRepos();
-        JSONArray array = jsonStringToArray(contentString);
-        System.out.println(array);
-    }
-
-    JSONArray jsonStringToArray(String jsonString) throws ParseException {
-        JSONParser parser = new JSONParser();
-        return (JSONArray)parser.parse(jsonString);
-    }
-
-    public String getUserRepos() throws IOException
-    {
-        URL url = new URL("https://api.github.com/user/repos");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.setRequestProperty("Authorization", getAuthHeaderValue());
-
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(connection.getInputStream()));
-
-        String inputLine;
-        StringBuilder content = new StringBuilder();
-        while ((inputLine = in.readLine()) != null) {
-            content.append(inputLine);
+    public void login(String login, String password) throws IOException {
+        JSONObject parameters = new JSONObject();
+        parameters.put("username", login);
+        parameters.put("password", password);
+        JSONObject res = callPostURL("http://localhost:3000/auth/login", parameters);
+        if(res == null){
+            throw new Error("An error have occurred while calling API");
         }
-
-        in.close();
-        return content.toString();
+        access_token = password;
+        user_name = login;
+        session = res.get("token").toString();
     }
 
-    private String getAuthHeaderValue()
-    {
-        String auth = this.USER_NAME + ":" + this.ACCESS_TOKEN;
-        byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(StandardCharsets.UTF_8));
-        return "Basic " + new String(encodedAuth);
+    public JSONObject callPostURL(String urlString, JSONObject parameters) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Accept", "application/json");
+
+        OutputStreamWriter osw = new OutputStreamWriter(connection.getOutputStream());
+        osw.write(parameters.toString());
+        osw.flush();
+        osw.close();
+
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            JSONParser parser = new JSONParser();
+            return (JSONObject) parser.parse(response.toString());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
