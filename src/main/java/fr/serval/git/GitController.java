6d5f21883f33314280b64cffab04d590b7798936
QuentinR <rouvilleq@gmail.com>
2021-05-03 21:33:07 +0200

    Refactoring GitController

--- src/main/java/fr/serval/git/GitController.java
+++ src/main/java/fr/serval/git/GitController.java
@@ -1,20 +1,13 @@
 package fr.serval.git;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
-
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
 public class GitController {
-    private String access_token;
-    private String session;
-    private String user_name;
-
     private static GitController instance;
+
+    private GitAuthController gitAuthController;
+
+    public GitController() {
+        gitAuthController = new GitAuthController();
+    }
 
     public static GitController getInstance() {
         if (isInstanceNotInitialized()) {
@@ -38,48 +31,7 @@ public class GitController {
         return instance == null;
     }
 
-    public void login(String login, String password) throws IOException {
-        JSONObject parameters = new JSONObject();
-        parameters.put("username", login);
-        parameters.put("password", password);
-        JSONObject res = callPostURL("http://localhost:3000/auth/login", parameters);
-        if(res == null){
-            throw new Error("An error have occurred while calling API");
-        }
-        access_token = password;
-        user_name = login;
-        session = res.get("token").toString();
-    }
-
-    public JSONObject callPostURL(String urlString, JSONObject parameters) throws IOException {
-        URL url = new URL(urlString);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("POST");
-        connection.setDoOutput(true);
-        connection.setRequestProperty("Content-Type", "application/json");
-        connection.setRequestProperty("Accept", "application/json");
-
-        OutputStreamWriter osw = new OutputStreamWriter(connection.getOutputStream());
-        osw.write(parameters.toString());
-        osw.flush();
-        osw.close();
-
-        try(BufferedReader br = new BufferedReader(
-                new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
-            StringBuilder response = new StringBuilder();
-            String responseLine = null;
-            while ((responseLine = br.readLine()) != null) {
-                response.append(responseLine.trim());
-            }
-            JSONParser parser = new JSONParser();
-            return (JSONObject) parser.parse(response.toString());
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public String getSession() {
-        return session;
+    public GitAuthController getGitAuthController() {
+        return gitAuthController;
     }
 }
--- src/main/java/fr/serval/launcher/ihm/LoginController.java
+++ src/main/java/fr/serval/launcher/ihm/LoginController.java
@@ -1,5 +1,6 @@
 package fr.serval.launcher.ihm;
 
+import fr.serval.git.GitAuthController;
 import fr.serval.git.GitController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,9 +15,11 @@ public class LoginController {
 
     @FXML
     private void handleSignIn(ActionEvent actionEvent) throws IOException {
-        GitController.getInstance().login(loginUsername.getText(), loginToken.getText());
-        System.out.println(GitController.getInstance().getSession());
-        if(GitController.getInstance().getSession() != null){
+        GitAuthController gitAuthController = GitController.getInstance().getGitAuthController();
+
+        gitAuthController.login(loginUsername.getText(), loginToken.getText());
+        System.out.println(gitAuthController.getSession());
+        if(gitAuthController.getSession() != null){
             LoginView.closeStage();
         }
     }
