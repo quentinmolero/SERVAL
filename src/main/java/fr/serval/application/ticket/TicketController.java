873f7ea0a206030a60e7e9bd5dea8fd93a6730d8
Quentin MOLERO <quentin.molero@estiam.com>
2021-05-06 21:21:45 +0200

    Add view for ticket

--- src/main/java/fr/serval/application/ticket/TicketController.java
+++ src/main/java/fr/serval/application/ticket/TicketController.java
@@ -1,16 +1,52 @@
 package fr.serval.application.ticket;
 
+import fr.serval.application.ticket.ihm.TicketListView;
+import fr.serval.application.ticket.ihm.TicketMainView;
+import fr.serval.application.ticket.ihm.TicketTopView;
 import fr.serval.controller.Controller;
 import fr.serval.ihm.IHMComponentBuilder;
+import fr.serval.tools.GridPaneConstraintBuilder;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 
 public class TicketController implements Controller, IHMComponentBuilder {
-    @Override
-    public void setupComponent() {
+    private final GridPane ticketView;
+    private final GridPane ticketCenterView;
 
+    private final TicketTopView ticketTopView;
+    private final TicketListView ticketListView;
+    private final TicketMainView ticketMainView;
+
+    public TicketController() {
+        this.ticketView = new GridPane();
+        this.ticketCenterView = new GridPane();
+
+        this.ticketTopView = new TicketTopView();
+        this.ticketListView = new TicketListView();
+        this.ticketMainView = new TicketMainView();
+
+        this.setupComponent();
     }
 
     @Override
-    public Object getComponent() {
-        return null;
+    public void setupComponent() {
+        this.ticketView.add(this.ticketTopView.getComponent(), 0, 0);
+        this.ticketView.add(this.ticketCenterView, 0, 1);
+
+        this.ticketView.getColumnConstraints().add(0, GridPaneConstraintBuilder.buildGridColumnConstraint(Priority.SOMETIMES, 100));
+        this.ticketView.getRowConstraints().add(0, GridPaneConstraintBuilder.buildGridRowConstraint(Priority.SOMETIMES, 20));
+        this.ticketView.getRowConstraints().add(1, GridPaneConstraintBuilder.buildGridRowConstraint(Priority.SOMETIMES, 80));
+
+        this.ticketCenterView.add(this.ticketListView.getComponent(), 0, 0);
+        this.ticketCenterView.add(this.ticketMainView.getComponent(), 1, 0);
+
+        this.ticketCenterView.getColumnConstraints().add(0, GridPaneConstraintBuilder.buildGridColumnConstraint(Priority.SOMETIMES, 25));
+        this.ticketCenterView.getColumnConstraints().add(1, GridPaneConstraintBuilder.buildGridColumnConstraint(Priority.SOMETIMES, 75));
+        this.ticketCenterView.getRowConstraints().add(0, GridPaneConstraintBuilder.buildGridRowConstraint(Priority.SOMETIMES, 100));
+    }
+
+    @Override
+    public GridPane getComponent() {
+        return this.ticketView;
     }
 }
---
+++ src/main/java/fr/serval/application/ticket/ihm/TicketMainView.java
@@ -0,0 +1,63 @@
+package fr.serval.application.ticket.ihm;
+
+import fr.serval.ihm.IHMComponentBuilder;
+import fr.serval.tools.GridPaneConstraintBuilder;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.text.Text;
+
+public class TicketMainView implements IHMComponentBuilder {
+    private final GridPane ticketMainView;
+    private final GridPane ticketInfoView;
+
+    private final Text ticketTitle;
+    private final Text ticketDescription;
+
+    private final Button closeTicketButton;
+
+    private final TicketDetailView ticketDetailView;
+
+    public TicketMainView() {
+        this.ticketMainView = new GridPane();
+        this.ticketInfoView = new GridPane();
+
+        this.ticketTitle = new Text();
+        this.ticketDescription = new Text();
+
+        this.closeTicketButton = new Button();
+
+        this.ticketDetailView = new TicketDetailView();
+
+        this.setupComponent();
+    }
+
+    @Override
+    public void setupComponent() {
+        this.ticketTitle.setText("ticket001");
+        this.ticketDescription.setText("Probleme sur l'IHM principale, les couleurs de texte et de fond sont inversees");
+
+        this.closeTicketButton.setText("Fermer le ticket");
+
+        this.ticketMainView.add(this.ticketInfoView, 0, 0);
+
+        this.ticketMainView.getColumnConstraints().add(0, GridPaneConstraintBuilder.buildGridColumnConstraint(Priority.SOMETIMES, 100));
+        this.ticketMainView.getRowConstraints().add(0, GridPaneConstraintBuilder.buildGridRowConstraint(Priority.SOMETIMES, 100));
+
+        this.ticketInfoView.add(this.ticketTitle, 0, 0);
+        this.ticketInfoView.add(this.ticketDescription, 0, 1);
+        this.ticketInfoView.add(this.ticketDetailView.getComponent(), 0, 2);
+        this.ticketInfoView.add(this.closeTicketButton, 0, 3);
+
+        this.ticketInfoView.getColumnConstraints().add(0, GridPaneConstraintBuilder.buildGridColumnConstraint(Priority.SOMETIMES, 100));
+        this.ticketInfoView.getRowConstraints().add(0, GridPaneConstraintBuilder.buildGridRowConstraint(Priority.SOMETIMES, 5));
+        this.ticketInfoView.getRowConstraints().add(1, GridPaneConstraintBuilder.buildGridRowConstraint(Priority.SOMETIMES, 5));
+        this.ticketInfoView.getRowConstraints().add(2, GridPaneConstraintBuilder.buildGridRowConstraint(Priority.SOMETIMES, 80));
+        this.ticketInfoView.getRowConstraints().add(3, GridPaneConstraintBuilder.buildGridRowConstraint(Priority.SOMETIMES, 10));
+    }
+
+    @Override
+    public GridPane getComponent() {
+        return this.ticketMainView;
+    }
+}
