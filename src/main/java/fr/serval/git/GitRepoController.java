f8a5c2bf5066e7266d98bbfbb01d69ecab5931e7
QuentinR <rouvilleq@gmail.com>
2021-05-07 19:48:30 +0200

    Add getProjetTaskGroup method

--- src/main/java/fr/serval/git/GitRepoController.java
+++ src/main/java/fr/serval/git/GitRepoController.java
@@ -12,8 +12,14 @@ public class GitRepoController
         GitAuthController gitAuthController = GitController.getInstance().getGitAuthController();
         return (JSONArray) RouteController.callGetURLWithBearerToken("projects/users/all", gitAuthController.getSession());
     }
+
     public JSONObject getProjetCommit(String projetName) throws IOException, ParseException {
         GitAuthController gitAuthController = GitController.getInstance().getGitAuthController();
         return (JSONObject) RouteController.callGetURLWithBearerToken("projects/" + projetName + "/commits", gitAuthController.getSession());
+    }
+
+    public JSONArray getProjetTaskGroup(int projetId) throws IOException, ParseException {
+        GitAuthController gitAuthController = GitController.getInstance().getGitAuthController();
+        return (JSONArray) RouteController.callGetURLWithBearerToken("projects/" + projetId + "/taskGroups", gitAuthController.getSession());
     }
 }
