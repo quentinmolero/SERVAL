ced688396740bb601490d803b39461fe566baf2b
QuentinR <rouvilleq@gmail.com>
2021-05-15 22:12:21 +0200

    Setup TaskGroupMenu

--- src/main/java/fr/serval/api/TaskGroupMenu.java
+++ src/main/java/fr/serval/api/TaskGroupMenu.java
@@ -1,11 +1,33 @@
 package fr.serval.api;
 
 import fr.serval.cli.CLIController;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 
 public class TaskGroupMenu {
     public static void main(int projectId){
+        int userAnswer;
+        do {
+            System.out.println("Que souhaitez vous faire ?");
+            System.out.println("1 : Ajouter un groupe de tâches");
+            System.out.println("2 : Voir les groupes de tâches");
+            System.out.println("3 : Retourner aux autres projets");
+            userAnswer = CLIController.readUserChoice(1, 3);
+
+            switch(userAnswer){
+                case 1:
+                    addTaskGroupMenu(projectId);
+                    break;
+                case 2:
+                    selectTaskGroupMenu(projectId);
+                    break;
+            }
+        } while(userAnswer != 3);
+    }
+
+    private static void addTaskGroupMenu(int projectId){
+        System.out.println("WIP");
+    }
+
+    private static void selectTaskGroupMenu(int projectId){
         System.out.println("WIP");
     }
 }
