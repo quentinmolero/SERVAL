c68cd78e369fb7bfef2bbd389fca261129014670
Quentin MOLERO <quentin.molero@estiam.com>
2021-05-23 18:54:00 +0200

    Add new task to the project form the UI and update task list

--- src/main/java/fr/serval/application/task/ihm/TaskListView.java
+++ src/main/java/fr/serval/application/task/ihm/TaskListView.java
@@ -2,8 +2,10 @@ package fr.serval.application.task.ihm;
 
 import fr.serval.api.APIController;
 import fr.serval.ihm.IHMComponentBuilder;
+import fr.serval.tools.GridPaneConstraintBuilder;
 import fr.serval.tools.JSONTools;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.*;
@@ -15,16 +17,23 @@ import java.util.HashMap;
 import java.util.List;
 
 public class TaskListView implements IHMComponentBuilder {
+    private final GridPane taskListBox;
     private final TreeView<String> tasksTree;
     private final TreeItem<String> tasksRoot;
+    private final Button addTask;
 
     private final HashMap<String, JSONObject> taskHashMap;
 
     private final TaskDetailsView taskDetailsView;
 
+    private int projectId;
+    private int taskCategoriesId;
+
     public TaskListView(TaskDetailsView taskDetailsView) {
+        this.taskListBox = new GridPane();
         this.tasksTree = new TreeView<>();
         this.tasksRoot = new TreeItem<>();
+        this.addTask = new Button();
 
         this.taskHashMap = new HashMap<>();
 
@@ -45,12 +54,27 @@ public class TaskListView implements IHMComponentBuilder {
             JSONObject selectedTask = this.taskHashMap.get(selectedNode.getValue());
             this.taskDetailsView.updateDetails(selectedTask);
         });
+
+        this.addTask.setText("Ajouter une tache");
+        this.addTask.setDisable(true);
+        this.addTask.setOnAction(event -> {
+            final NewTaskDialog newTaskDialog = new NewTaskDialog(this, this.projectId, this.taskCategoriesId);
+            newTaskDialog.getComponent().show();
+        });
+
+        this.taskListBox.add(this.tasksTree, 0, 0);
+        this.taskListBox.add(this.addTask, 0, 1);
+        this.taskListBox.getColumnConstraints().add(GridPaneConstraintBuilder.buildGridColumnConstraint(Priority.SOMETIMES, 100));
+        this.taskListBox.getRowConstraints().add(GridPaneConstraintBuilder.buildGridRowConstraint(Priority.SOMETIMES, 80));
+        this.taskListBox.getRowConstraints().add(GridPaneConstraintBuilder.buildGridRowConstraint(Priority.SOMETIMES, 20));
     }
 
     public void updateTasks(int projectId, int taskCategoriesId) {
         this.taskHashMap.clear();
         this.tasksRoot.getChildren().clear();
-        JSONArray groupTasks = APIController.getInstance().getAPITaskController().getAllTaskForATaskGroup(projectId, taskCategoriesId);
+        this.projectId = projectId;
+        this.taskCategoriesId = taskCategoriesId;
+        JSONArray groupTasks = APIController.getInstance().getAPITaskController().getAllTaskForATaskGroup(this.projectId, this.taskCategoriesId);
         List<JSONObject> tasks = JSONTools.collectJSONArrayChildrenAsArrayList(groupTasks);
         for (JSONObject task : tasks) {
             String taskName = JSONTools.extractStringFromJSONObject(task, "name");
@@ -58,10 +82,11 @@ public class TaskListView implements IHMComponentBuilder {
             this.taskHashMap.put(taskName, task);
             this.tasksRoot.getChildren().add(new TreeItem<>(taskName));
         }
+        this.addTask.setDisable(false);
     }
 
     @Override
-    public TreeView<String> getComponent() {
-        return this.tasksTree;
+    public GridPane getComponent() {
+        return this.taskListBox;
     }
 }
