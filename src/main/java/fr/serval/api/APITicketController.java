23d4f5c343160dfd75cbf52e3efa639847efd1c7
QuentinR <rouvilleq@gmail.com>
2021-06-20 21:46:28 +0200

    Add route to APITicketController to add a ticket

--- src/main/java/fr/serval/api/APITicketController.java
+++ src/main/java/fr/serval/api/APITicketController.java
@@ -11,4 +11,13 @@ public class APITicketController
         APIAuthController APIAuthController = APIController.getInstance().getAPIAuthController();
         return (JSONArray) APIRouter.callGetURLWithBearerToken(ROUTE + "projects/"+project_id, null, APIAuthController.getSession());
     }
+
+    public JSONObject addTicketToAProjet(int project_id, String title, String description) {
+        APIAuthController APIAuthController = APIController.getInstance().getAPIAuthController();
+        JSONObject parameters = new JSONObject();
+        parameters.put("projectId", project_id);
+        parameters.put("title", title);
+        parameters.put("description", description);
+        return (JSONObject) APIRouter.callPostURLWithBearerToken(ROUTE + "projects", parameters, APIAuthController.getSession());
+    }
 }
