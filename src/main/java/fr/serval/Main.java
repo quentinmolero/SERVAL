7567d0c1bb93e6296f63ea36ee35705aa9c51843
Quentin MOLERO <quentin.molero@estiam.com>
2021-06-25 21:31:00 +0200

    Prepared maven file for jar file and handle CLI launch with 'cli' or '-c'

--- src/main/java/fr/serval/Main.java
+++ src/main/java/fr/serval/Main.java
@@ -1,10 +1,16 @@
 package fr.serval;
 
+import fr.serval.cli.LauncherMenu;
 import fr.serval.launcher.Launcher;
 
 public class Main
 {
     public static void main(String[] args) {
-        Launcher.main();
+        System.out.println(args.length);
+        if (args.length > 0 && (args[0].equals("cli") || args[0].equals("-c"))) {
+            LauncherMenu.main();
+        } else {
+            Launcher.main();
+        }
     }
 }
