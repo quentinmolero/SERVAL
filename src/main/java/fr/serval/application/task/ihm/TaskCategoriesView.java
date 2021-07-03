e4e07c0065628c3bde790b2e7945e688cdb41bf7
Quentin MOLERO <quentin.molero@estiam.com>
2021-07-03 15:02:01 +0200

    Add possibility to add task categories from interface

--- src/main/java/fr/serval/application/task/ihm/TaskCategoriesView.java
+++ src/main/java/fr/serval/application/task/ihm/TaskCategoriesView.java
@@ -3,8 +3,10 @@ package fr.serval.application.task.ihm;
 import fr.serval.api.APIController;
 import fr.serval.application.project.Project;
 import fr.serval.ihm.IHMComponentBuilder;
+import fr.serval.tools.GridPaneConstraintBuilder;
 import fr.serval.tools.JSONTools;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.*;
@@ -15,8 +17,10 @@ import java.util.HashMap;
 import java.util.List;
 
 public class TaskCategoriesView implements IHMComponentBuilder {
+    private final GridPane taskCategoriesBox;
     private final TreeView<String> categoriesTree;
     private final TreeItem<String> categoriesRoot;
+    private final Button addCategoriesTask;
 
     private final HashMap<String, Integer> taskCategoriesHashMap;
 
@@ -24,8 +28,10 @@ public class TaskCategoriesView implements IHMComponentBuilder {
     private final Project project;
 
     public TaskCategoriesView(TaskListView taskListView, Project project) {
+        this.taskCategoriesBox = new GridPane();
         this.categoriesTree = new TreeView<>();
         this.categoriesRoot = new TreeItem<>();
+        this.addCategoriesTask = new Button();
 
         this.taskCategoriesHashMap = new HashMap<>();
 
@@ -49,10 +55,23 @@ public class TaskCategoriesView implements IHMComponentBuilder {
             this.taskListView.updateTasks(this.project.getId(), selectedCategories);
         });
 
+        this.addCategoriesTask.setText("Ajouter une tache");
+        this.addCategoriesTask.setOnAction(event -> {
+            final NewCategoriesDialog newCategoriesDialog = new NewCategoriesDialog(this, this.project.getId());
+            newCategoriesDialog.getComponent().show();
+        });
+
+        this.taskCategoriesBox.add(this.categoriesTree, 0, 0);
+        this.taskCategoriesBox.add(this.addCategoriesTask, 0, 1);
+        this.taskCategoriesBox.getColumnConstraints().add(GridPaneConstraintBuilder.buildGridColumnConstraint(Priority.SOMETIMES, 100));
+        this.taskCategoriesBox.getRowConstraints().add(GridPaneConstraintBuilder.buildGridRowConstraint(Priority.SOMETIMES, 80));
+        this.taskCategoriesBox.getRowConstraints().add(GridPaneConstraintBuilder.buildGridRowConstraint(Priority.SOMETIMES, 20));
+
         this.updateCategories();
     }
 
     public void updateCategories() {
+        this.taskCategoriesHashMap.clear();
         JSONObject projectTaskGroup = (JSONObject) APIController.getInstance().getAPIProjectController().getProjetTaskGroup(this.project.getId()).get(0);
         List<JSONObject> taskCategories = JSONTools.collectJSONArrayChildrenAsArrayList(JSONTools.extractJSONArrayFromJSONObject(projectTaskGroup, "Task_Groups"));
         for (JSONObject categories : taskCategories) {
@@ -65,7 +84,7 @@ public class TaskCategoriesView implements IHMComponentBuilder {
     }
 
     @Override
-    public TreeView<String> getComponent() {
-        return this.categoriesTree;
+    public GridPane getComponent() {
+        return this.taskCategoriesBox;
     }
 }
